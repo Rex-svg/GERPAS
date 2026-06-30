cb6014cba463f775db1b89fd992ce1bb14c819cb
Ryuzaki <106133768+Rex-svg@users.noreply.github.com>
2026-06-30 12:54:01 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
+  /* config options here */
   reactCompiler: true,
-
-  // Required for GitHub Pages
-  output: "export",
-
-  // Disable Next.js image optimization
-  images: {
-    unoptimized: true,
-  },
-
-  // Replace "YOUR_REPOSITORY_NAME" with your GitHub repository name
-  basePath: isProd ? "/GERPAS/" : "",
-  assetPrefix: isProd ? "/GERPAS/" : "",
 };
 
 export default nextConfig;
